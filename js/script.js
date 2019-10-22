eab1a971410ae9d8539dc4c73f38450c85288021
Lexsys207 <nano.soldat@ya.ru>
2019-10-22 09:33:58 +0300

    3-я секция
    Верстка 3-й секции, так же добавлен js код для переключения информации о персонале.

--- js/script.js
+++ js/script.js
@@ -1,4 +1,23 @@
-var duration = 600; //'slow'
-$(".item").each(function(index) {
-    $(this).delay(duration * index).fadeIn(duration);
-});
+var item_1 = document.getElementById("person-1");
+var item_2 = document.getElementById("person-2");
+var item_3 = document.getElementById("person-3");
+
+function show_1() {
+    item_1.classList.add("show");
+    item_2.classList.remove("show");
+    item_3.classList.remove("show");
+}
+function show_2() {
+    item_1.classList.remove("show");
+    item_2.classList.add("show");
+    item_3.classList.remove("show");
+}
+function show_3() {
+    item_1.classList.remove("show");
+    item_2.classList.remove("show");
+    item_3.classList.add("show");
+}
+
+item_1.addEventListener('click', show_1);
+item_2.addEventListener('click', show_2);
+item_3.addEventListener('click', show_3);
